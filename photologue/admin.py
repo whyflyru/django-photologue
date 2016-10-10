164f116a65b42f78c4bbef558ecd5132fbabd7c9
Adrian Tangochin <tangochin@yandex.ru>
2016-10-10 14:43:48 +0500

    Easy support of subdomains

--- photologue/admin.py
+++ photologue/admin.py
@@ -27,7 +27,7 @@ class GalleryAdminForm(forms.ModelForm):
 
 
 class GalleryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date_added', 'photo_count', 'is_public')
+    list_display = ('title', 'subdomain', 'date_added', 'photo_count', 'is_public')
     list_filter = ['date_added', 'is_public']
     if MULTISITE:
         list_filter.append('sites')
